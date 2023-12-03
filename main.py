54b30207456e174dc1c5aa14035bea9583a641dd
nic <119163097+toastedpumpkin@users.noreply.github.com>
2023-12-03 23:54:26 +0000

    Update main.py
    IT DOESNT SAY SUS BRO IT SAYS SWS PLEASE TRUST ME

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import requests
 import time
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(f"{Fore.MAGENTA}________           ___       __           ________ ")           
+print(f"{Fore.MAGENTA}|\   ____\         |\  \     |\  \        |\   ____\ ")
+print(f"{Fore.MAGENTA}\ \  \___|_        \ \  \    \ \  \       \ \  \___|_ ")
+print(f"{Fore.MAGENTA} \ \_____  \        \ \  \  __\ \  \       \ \_____  \   ")
+print(f"{Fore.MAGENTA}  \|____|\  \        \ \  \|\__\_\  \       \|____|\  \  ")
+print(f"{Fore.MAGENTA}    ____\_\  \        \ \____________\        ____\_\  \ ")
+print(f"{Fore.MAGENTA}  |\_________\        \|____________|       |\_________\ ")
+    
+time.sleep(3)
 
 wbhk = input("webhook: ")
 pyld = input("payload: ")
