a8e3e947f3a5cdc8a121abb566df23bd30add407
nic <119163097+toastedpumpkin@users.noreply.github.com>
2023-12-04 21:54:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -14,18 +14,21 @@ print(f"{Fore.MAGENTA} ░ ▒  ▒      ▒ ░ ░     ░ ░▒  ░ ░")
 print(f"{Fore.MAGENTA} ░ ░  ░      ░   ░     ░  ░  ░  ")  
 print(f"{Fore.MAGENTA}   ░           ░             ░  ")
 print(f"{Fore.MAGENTA} ░ ")
-time.sleep(1)
+time.sleep(0.3)
 print(f"{Fore.MAGENTA}https://discord.gg/cHzvPFHPVd")
-time.sleep(2)
+time.sleep(0.7)
 
 wbhk = input(f"{Fore.LIGHTWHITE_EX}> webhook: ")
 pyld = input(f"{Fore.LIGHTWHITE_EX}> payload: ")
+lmao = ("awesome sauce 😈, you've just been webhook spammed")
 print(f"{Fore.RED}awesome sauce")
+
+requests.post(wbhk,json={'content': lmao,})
 
 def function():
     requests.post(wbhk,json={'content': pyld,})
 
 while True:
     function()
-    time.sleep(0.6)
+    time.sleep(0.2)
     print(f"{Fore.GREEN}message sent")
