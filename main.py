1e1706cb1a5b37113ad620958165e9de6540b935
nic <119163097+toastedpumpkin@users.noreply.github.com>
2023-12-06 12:36:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
-pcname = (os.getenv('COMPUTERNAME'))
-os.system('title DWS by toastedpumpkin - thanks for using this tool ' + pcname)
+os.system('title DWS by toastedpumpkin - session not logged in yet')
+login = input(f"{Fore.LIGHTWHITE_EX}> Username: ")
+os.system('title DWS by toastedpumpkin - logged in as ' + login)
+print(f"{Fore.GREEN}[+] Successfully logged in.")
+print(f"{Fore.LIGHTYELLOW_EX}Loading DWS...")
+time.sleep(2)
+os.system('cls')
 
 print(f"{Fore.MAGENTA} █████▄     █     █░     ██████ ")           
 print(f"{Fore.MAGENTA}▒██▀ ██▌   ▓█░ █ ░█░   ▒██    ▒ ")
@@ -19,11 +24,14 @@ print(f"{Fore.MAGENTA} ░ ░  ░      ░   ░     ░  ░  ░  ")
 print(f"{Fore.MAGENTA}   ░           ░             ░  ")
 print(f"{Fore.MAGENTA} ░ ")
 time.sleep(0.3)
-print(f"{Fore.MAGENTA}by toastedpumpkin - https://discord.gg/cHzvPFHPVd")
+if login == "toastedpumpkin" or  login == "nic":
+    print(f"{Fore.LIGHTMAGENTA_EX}Welcome Back, Master.")
+if login == "Paw" or login == "paw":
+    print(f"{Fore.LIGHTMAGENTA_EX}lemons and demons LMAOO")
 time.sleep(0.5)
 
-wbhk = input(f"{Fore.LIGHTWHITE_EX}> webhook: ")
-pyld = input(f"{Fore.LIGHTWHITE_EX}> payload: ")
+wbhk = input(f"{Fore.LIGHTWHITE_EX}> Webhook: ")
+pyld = input(f"{Fore.LIGHTWHITE_EX}> Payload: ")
 print(f"{Fore.RED}[+] Awesome sauce.")
 
 def function():
